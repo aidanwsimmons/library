d0401a446d49e73dfe45e1f429905f376015e2c4
Aidan Simmons <aidanwsimmons621@gmail.com>
2024-06-05 15:38:49 -0700

    added functions to remove/add books, display Library, and change read status

--- app.js
+++ app.js
@@ -8,28 +8,41 @@ function Book(title, author, numberOfPages, hasBeenRead) {
 }
 
 //adds book to library
-function addBookToLibrary() {
-
+function addBookToLibrary(book) {
+    myLibrary.push(book)
 }
 
 //shows library as individual book cards
-function displayBooksInLibrary() {
-
+function displayLibrary() {
+    myLibrary.forEach((book, index) => {
+        console.log(`${index + 1}. Title: ${book.title}, Author: ${book.author}, Pages: ${book.numberOfPages}, Read: ${book.hasBeenRead}`);
+    })
 }
 
-
-function removeBookFromLibrary() {
-
+//removes book from library at given index
+function removeBookFromLibrary(index) {
+    if(index >= 0 && index < myLibrary.length){
+        myLibrary.splice(index, 1)
+    } else {
+        console.log("Invalid index")
+    }
 }
 
-function updateBookStatus() {
-
+//changes hasBeenRead property of book at given index
+function toggleReadStatus(index) {
+    if (index >= 0 && index < myLibrary.length) {
+        myLibrary[index].hasBeenRead = !myLibrary[index].hasBeenRead;
+    } else {
+        console.log("Invalid index");
+    }
 }
 
+//shows overlay + addBook form when addBook button is clicked
 document.querySelector('.addBookBtn').addEventListener('click', function() {
     document.getElementById('formOverlay').classList.add('show');
 });
 
+//hides overlay when anywhere outside the form is clicked
 document.getElementById('formOverlay').addEventListener('click', function(event) {
     if (event.target === this) {
         this.classList.remove('show');
