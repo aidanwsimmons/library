b2468643980e6a686285e86558d4421ebe7c62a1
Aidan Simmons <aidanwsimmons621@gmail.com>
2024-06-05 15:55:27 -0700

    expanded displayLibrary function to dynamically create book card elements

--- app.js
+++ app.js
@@ -7,33 +7,64 @@ function Book(title, author, numberOfPages, hasBeenRead) {
     this.hasBeenRead = hasBeenRead;
 }
 
+//shows library as individual book cards
+function displayLibrary() {
+    const libraryDiv = document.querySelector('.library')
+    libraryDiv.innerHTML = ''
+
+    myLibrary.forEach((book, index) => {
+        const bookDiv = document.createElement('div')
+        bookDiv.classList.add('book')
+
+        const titleSpan = document.createElement('span')
+        titleSpan.textContent = `${book.title}`
+        bookDiv.appendChild(titleSpan)
+
+        const authorSpan = document.createElement('span')
+        titleSpan.textContent = `by ${book.author}`
+        bookDiv.appendChild(titleSpan)
+
+        const pagesSpan = document.createElement('span')
+        titleSpan.textContent = `${book.numberOfPages} pages`
+        bookDiv.appendChild(titleSpan)
+
+        const readButton = document.createElement('button')
+        readButton.textContent = book.hasBeenRead ? 'Read' : 'Not Read'
+        readButton.addEventListener('click', () => {
+            toggleReadStatus(index)
+        })
+        bookDiv.appendChild(readButton)
+
+        const removeButton = document.createElement('button')
+        removeButton.textContent = 'Remove'
+        removeButton.addEventListener('click', () => {
+            removeBookFromLibrary(index)
+        })
+        bookDiv.appendChild(removeButton)
+
+        libraryDiv.appendChild(bookDiv)
+    })
+}
+
 //adds book to library
 function addBookToLibrary(book) {
     myLibrary.push(book)
-}
-
-//shows library as individual book cards
-function displayLibrary() {
-    myLibrary.forEach((book, index) => {
-        console.log(`${index + 1}. Title: ${book.title}, Author: ${book.author}, Pages: ${book.numberOfPages}, Read: ${book.hasBeenRead}`);
-    })
+    displayLibrary()
 }
 
 //removes book from library at given index
 function removeBookFromLibrary(index) {
     if(index >= 0 && index < myLibrary.length){
         myLibrary.splice(index, 1)
-    } else {
-        console.log("Invalid index")
+        displayLibrary()
     }
 }
 
 //changes hasBeenRead property of book at given index
 function toggleReadStatus(index) {
     if (index >= 0 && index < myLibrary.length) {
-        myLibrary[index].hasBeenRead = !myLibrary[index].hasBeenRead;
-    } else {
-        console.log("Invalid index");
+        myLibrary[index].hasBeenRead = !myLibrary[index].hasBeenRead
+        displayLibrary()
     }
 }
 
